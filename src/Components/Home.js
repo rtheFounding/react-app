ab453e62c607cc3e9be663424cd35decadb83ce7
Fernando Sanchez <{ID}+{username}@users.noreply.github.com>
2023-12-05 17:57:04 -0500

    Create the file for the Header component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <section className="">
+        
         <div
           id="carouselExampleSlidesOnly"
           class="carousel slide"
