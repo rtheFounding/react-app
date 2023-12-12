d273d886afaeb1ebb63be0033d9e84c11bb2be33
Fernando Sanchez <49005233+fisanchez@users.noreply.github.com>
2023-12-12 17:55:13 -0500

    Render from hard coded restaraunt data

--- src/layouts/ResturantsLayout.js
+++ src/layouts/ResturantsLayout.js
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router-dom";
 
-export default function CareersLayout() {
+const data = {
+  "restaurants": [
+      {
+          "id": 1,
+          "name": "Taste of Italy",
+          "description": "Authentic Italian restaurant with a wide selection of pasta and pizza dishes.",
+          "location": "123 Main Street, Atlanta, USA",
+          "Rating": 4.6
+      },
+  ]
+}
+
+export default function ResturantLayout() {
   return (
     <div className="resturant-layout">
+      {JSON.stringify(data)}
       <h2>Resturants</h2>
       <p>
         Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
